484a8acd8ae8e9abf09ba771640735a921750591
adhdhdh <135920314+AdyGomezMtz@users.noreply.github.com>
2023-06-29 14:41:12 -0500

    agregar nueva clase y funcion para agregar a la base de datos

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace EmpleaedoM23CV
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
